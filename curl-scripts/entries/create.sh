fc656724fc5d3c661fa9287b3bfc4b36b78b9352
jtshepherd31 <jtshepherd31@gmail.com>
2021-06-23 11:42:15 -0600

    Re-add curls scripts

---
+++ curl-scripts/entries/create.sh
@@ -0,0 +1,14 @@
+API="http://localhost:4741"
+URL_PATH="/create-entry"
+
+curl "${API}${URL_PATH}" \
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+    "entry": {
+      "text": "'"${TEXT}"'",
+      "owner": "'"${OWNER}"'"
+    }
+  }'
